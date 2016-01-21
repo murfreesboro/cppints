3d5d498b725bfbfa03bf6205822e15eb1f6b2cd2
fenglai <fhilosophierr@gmail.com>
2016-01-21 16:05:52 -0600

    update mem_stat.py

--- util/mem_stat.py
+++ util/mem_stat.py
@@ -217,13 +217,154 @@ def checkMem(filename,workDir):
     # finally, let's return the nMem
     return nMem
 
+def getWorkPath(topDir,order):
+    """
+    return the work path through top dir
+    """
+    projectDir = topDir
+    if order == "0":
+        projectDir = topDir + "/" + "energy"
+    elif order == "1":
+        projectDir = topDir + "/" + "first_deriv"
+    elif order == "2":
+        projectDir = topDir + "/" + "second_deriv"
+    else:
+        print "illegal order here, we only support order from 0 to 2"
+        sys.exit()
+    return projectDir
+
+def checkJob(topDir,job,order):
+    """
+    check the input job, to see that whether
+    they are valid
+    also return the information about maxL
+    and auxMaxL
+    """
+    # firstly let's check the job
+    # also define the work dir
+    workDir = job
+    if job == "eri" or job == "2eri" or job == "3eri":
+        workDir = "eri"
+    elif job == "twobodyoverlap":
+        workDir = "twobodyoverlap"
+    elif job == "threebodyoverlap":
+        workDir = "threebodyoverlap"
+    elif job == "kinetic":
+        workDir = "kinetic"
+    elif job == "esp":
+        workDir = "esp"
+    elif job == "nai":
+        workDir = "nai"
+    elif job == "mom":
+        workDir = "mom"
+    else:
+        print job
+        print "illegal job here, please check the function of checkJob to see th supported operator list"
+        sys.exit()
+
+    # set the maxL and auxL
+    # in default they are 0, since they are not used
+    # however, for some given jobs, they need to be 
+    # examined
+    maxL = 0
+    auxL = 0
+    if job == "eri" or job == "2eri" or job == "3eri":
+
+        # we need to loop over the dir
+        projectDir = getWorkPath(topDir,order)
+        wDir = projectDir + "/eri" 
+        if not os.path.isdir(wDir):
+            print "you set the eri job, however there are no source code?"
+            sys.exit()
+
+        # now let's loop over the files to see L information
+        files = os.listdir(wDir)
+        for iFile in files:
+
+            # we only concentrate on main cpp file
+            if not isMainCPPFile(iFile):
+                continue
+
+            # get the L list
+            L_list = getLCode(iFile)
+
+            # now see the job
+            # for all of eri job
+            # we just check the ket2 position
+            # this is for setting maxL
+            # because ket2 is never referred by 2eri and 3eri
+            #
+            # for 2eri/3eri, we need to check KET1
+            # this is the direction of aux basis set
+            L = L_list[3]
+
+            # whether this is SP shell?
+            # then we need to reset it
+            ang = L
+            if L == 100:
+                ang = 1
+
+            # now get value
+            if ang>maxL:
+                maxL = ang
+
+            # for aux basis set
+            if job == "3eri" or job == "2eri":
+                L = L_list[2]
+                ang = L
+                if L == 100:
+                    ang = 1
+                if ang>auxL:
+                    auxL = ang
+
+    # now let's return
+    print "maxL is", maxL
+    print "auxMaxL is", auxL
+    return (maxL,auxL,workDir)
+
+def checkERIJob(job,maxL,auxL,filename):
+    """
+    for the given ERI job, through the information
+    of maxL and auxL, we check that whether we 
+    should include the given file
+    """
+
+    # we just test ERI job
+    if job == "eri" or job == "2eri" or job == "3eri":
+
+        # get the L information
+        # we also need to correct it
+        L = getLCode(filename)
+        for i in range(len(L)):
+            ang = L[i]
+            if ang == 100:
+                L[i] = 1
+
+        if L[0] == 4 and L[2] == 5:
+            print "we have the missing file"
+
+        # now let's see
+        if job == "eri":
+            if L[0] > maxL or L[1] > maxL or L[2] > maxL or L[3] > maxL:
+                return False
+        elif job == "2eri":
+            if L[0] > auxL or L[1] > 0    or L[2] > auxL or L[3] > 0:
+                return False
+        elif job == "3eri":
+            if L[0] > maxL or L[1] > maxL or L[2] > auxL or L[3] > 0:
+                return False
+
+    # now everything is good
+    # let's go on
+    return True
+
 #################################################
 # now this is main script for memory statistics #
 #################################################
 order = 0
 if len(sys.argv) == 4:
     topDir  = sys.argv[1]
-    wDir    = sys.argv[2]
+    job     = sys.argv[2]
     order   = sys.argv[3]
 else:
     print "We need three arguments, one is the input cpp files dir"
@@ -239,24 +380,26 @@ if topDir[-1] == "/":
     tmp = topDir[0:-1]
     topDir = tmp
 
+# further check the job
+# for some job, for example; the 
+# eri, 2eri and 3eri job; we need to 
+# search the maxL and auxMaxL
+maxL    = 0
+auxMaxL = 0
+wDir    = job
+(maxL, auxMaxL, wDir) = checkJob(topDir,job,order)
+
 # on the top of dir, we should have 
 # three sub-dir whose name like this
-projectDir = topDir
+projectDir = getWorkPath(topDir,order)
 suffix = "_0"
-if order == "0":
-    projectDir = topDir + "/" + "energy"
-elif order == "1":
-    projectDir = topDir + "/" + "first_deriv"
+if order == "1":
     suffix = "_1"
 elif order == "2":
-    projectDir = topDir + "/" + "second_deriv"
     suffix = "_2"
-else:
-    print "illegal order here, we only support order from 0 to 2"
-    sys.exit()
 
 # we should create file to store the result
-out = "mem_infor_" + wDir + suffix + ".txt"
+out = "mem_infor_" + job + suffix + ".txt"
 
 # now the real work
 projects = os.listdir(projectDir)
@@ -277,6 +420,10 @@ if gotIt:
         if iFile.find("cpp") < 0:
             continue
 
+        # for ERI job, we need a double check
+        if not checkERIJob(job,maxL,auxMaxL,iFile):
+            continue
+
         # now do the real job
         fname = workDir + "/" + iFile
         nMem = checkMem(fname,workDir)
@@ -288,13 +435,13 @@ if gotIt:
         code   = formLCode(L_list)
         output.write( "%-12d  %-12d " % (code, nMem))
         if len(L_list) == 1:
-            output.write( "# %-3d\n" % (L_list[0]))
+            output.write( " %-3d\n" % (L_list[0]))
         elif len(L_list) == 2:
-            output.write( "# %-3d  %-3d\n" % (L_list[0], L_list[1]))
+            output.write( " %-3d  %-3d\n" % (L_list[0], L_list[1]))
         elif len(L_list) == 3:
-            output.write( "# %-3d  %-3d  %-3d\n" % (L_list[0], L_list[1], L_list[2]))
+            output.write( " %-3d  %-3d  %-3d\n" % (L_list[0], L_list[1], L_list[2]))
         elif len(L_list) == 4:
-            output.write( "# %-3d  %-3d  %-3d  %-3d\n" % (L_list[0], L_list[1], L_list[2], L_list[3]))
+            output.write( " %-3d  %-3d  %-3d  %-3d\n" % (L_list[0], L_list[1], L_list[2], L_list[3]))
         else:
             print "something error after we getLCode"
             sys.exit()
